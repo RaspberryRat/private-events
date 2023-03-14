a0d82fe065cedd62c68d9df8faa276945c4ec232
G <mail@grahamirvine.com>
2023-03-14 12:08:29 -0600

    changed enrollment reference to integer, and added index

--- db/migrate/20230314174344_create_enrollments.rb
+++ db/migrate/20230314174344_create_enrollments.rb
@@ -1,8 +1,8 @@
 class CreateEnrollments < ActiveRecord::Migration[7.0]
   def change
     create_table :enrollments do |t|
-      t.references :event, null: false, foreign_key: true
-      t.references :creator, null: false, foreign_key: true
+      t.integer :event, null: false, foreign_key: true, index: { unique: true }
+      t.integer :creator, null: false, foreign_key: true, index: { unique: true }
       t.string :category
 
       t.timestamps
