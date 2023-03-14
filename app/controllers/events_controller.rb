545508955307eb28f34f3d8429ecc166aa598ac1
G <mail@grahamirvine.com>
2023-03-14 14:39:18 -0600

    setup methods in event_controller, untested

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,28 @@
 class EventsController < ApplicationController
-  def index
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @event = Event.new
   end
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Event.current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :date)
+  end
+
 end
