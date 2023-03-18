d57ca5caca55783a4d57927a35dcd44b43c9b7d8
G <mail@grahamirvine.com>
2023-03-18 15:20:57 -0600

    class method and updated view to show upcoming and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
 
   has_many :admissions, foreign_key: :event_attended_id
   has_many :attendees, through: :admissions
+
+  def self.past
+    self.where("date < ?", Time.now)
+  end
+
+  def self.future
+    self.where("date > ?", Time.now)
+  end
 end
