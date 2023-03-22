c5fb0452f1a83108013ad6a7aaad0d1600380e83
G <mail@grahamirvine.com>
2023-03-22 16:26:57 -0600

    working on invite controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,2 +1,21 @@
 class InvitesController < ApplicationController
+  def new
+    @invite = Invite.new
+
+    redirect_to root_path
+  end
+
+  def create
+    @invite = Invite.new
+    redirect_to root_path
+  end
 end
+
+# @event = Event.find(params[:id])
+    # if @event.attendees.include?(current_user)
+    #   flash[:error] = "You are already attending: #{@event.title}"
+    # else
+    #   @event.attendees << current_user
+    #   flash[:success] = "You are now attending happening: #{@event.title}"
+    # end
+    # redirect_to event_path(@event.id)
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
 
   resources :admissions, only: [:new, :destroy]
 
+  resources :invites, only: [:new, :create]
+
 end
