ee86b9d54f7d2f2aae42098013531629d64f7aca
G <mail@grahamirvine.com>
2023-03-23 16:35:51 -0600

    fixed typo for :acitve

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -8,17 +8,8 @@ class InvitesController < ApplicationController
     @user = User.find(params[:user_id])
 
     @event.invitees << @user
-
     flash[:success] = "#{@user.user_name} is now invited."
+
     redirect_to event_path(@event.id)
   end
 end
-
-# @event = Event.find(params[:id])
-    # if @event.attendees.include?(current_user)
-    #   flash[:error] = "You are already attending: #{@event.title}"
-    # else
-    #   @event.attendees << current_user
-    #   flash[:success] = "You are now attending happening: #{@event.title}"
-    # end
-    # redirect_to event_path(@event.id)
